8417f4470c5d288c4ad0086ae894b3bbbf592d14
ADAWAS <pasitsupholkhan@gmail.com>
2021-11-21 13:32:51 +0700

    imwrite

--- opencv.py
+++ opencv.py
@@ -1,5 +1,7 @@
 import cv2
-img = cv2.imread('image/wow.jpg')
+
+img = cv2.imread('image/wow.jpg', 1)
+# 1 color, 0 grayscale
 
 # read img
 print(img.ndim)
@@ -7,7 +9,10 @@ print(type(img.ndim))
 print(img)
 
 # img resize
-imgresize = cv2.resize(img, (600, 600))
+imgresize = cv2.resize(img, (900, 600))
+
+# img write
+cv2.imwrite('imwrite.jpg', imgresize)
 
 # show img
 cv2.imshow('TITLE', imgresize)
