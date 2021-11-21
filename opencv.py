7bd33693a709f56b23cd428b0d4a9941af407aaa
ADAWAS <pasitsupholkhan@gmail.com>
2021-11-21 13:27:03 +0700

    resize img

--- opencv.py
+++ opencv.py
@@ -1,3 +1,15 @@
 import cv2
 img = cv2.imread('image/wow.jpg')
+
+# read img
+print(img.ndim)
+print(type(img.ndim))
 print(img)
+
+# img resize
+imgresize = cv2.resize(img, (600, 600))
+
+# show img
+cv2.imshow('TITLE', imgresize)
+cv2.waitKey(delay=2000)
+cv2.destroyAllwindow()
